d656153c26022bbc16fa5233effe0cac93627f96
AnasMarzouq <63076945+AnasMeshal@users.noreply.github.com>
2021-07-31 15:54:53 +0300

    Update index.js

--- Client/index.js
+++ Client/index.js
@@ -1,4 +1,4 @@
-const socket = io("http://localhost:5000");
+const socket = io({ transports: ["websocket"], upgrade: false });
 
 //!DOM
 const parentDiv = document.getElementById("message-container");
